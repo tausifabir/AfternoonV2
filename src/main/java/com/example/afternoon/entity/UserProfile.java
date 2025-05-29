93d898b0c2b3aabb0137665e1f262e4bb7461088
tausifabir <tausif.islam@bracits.com>
2025-05-29 15:53:07 +0600

    refactor: user

--- src/main/java/com/example/afternoon/entity/UserProfile.java
+++ src/main/java/com/example/afternoon/entity/UserProfile.java
@@ -1,16 +1,28 @@
 package com.example.afternoon.entity;
 
-import com.example.afternoon.dto.UserDto;
 import com.example.afternoon.enumeration.Status;
 import com.sun.istack.NotNull;
-import org.springframework.data.relational.core.mapping.Table;
-
-import javax.persistence.*;
 import java.io.Serializable;
 import java.util.Date;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.data.relational.core.mapping.Table;
 
+
+
+
+@Setter
+@Getter
 @Entity
 @Table("user_profile")
+@ToString
 public class UserProfile implements Serializable {
 
   @Id
@@ -56,158 +68,5 @@ public class UserProfile implements Serializable {
   @ManyToOne
   @JoinColumn(name = "updated_by_id")
   private UserProfile updatedBy;
-
-
-  public Long getId() {
-    return id;
-  }
-
-  public void setId(Long id) {
-    this.id = id;
-  }
-
-  public String getUserName() {
-    return userName;
-  }
-
-  public void setUserName(String userName) {
-    this.userName = userName;
-  }
-
-  public String getPin() {
-    return pin;
-  }
-
-  public void setPin(String pin) {
-    this.pin = pin;
-  }
-
-  public String getPhone() {
-    return phone;
-  }
-
-  public void setPhone(String phone) {
-    this.phone = phone;
-  }
-
-  public String getEmail() {
-    return email;
-  }
-
-  public void setEmail(String email) {
-    this.email = email;
-  }
-
-  public String getDepartment() {
-    return department;
-  }
-
-  public void setDepartment(String department) {
-    this.department = department;
-  }
-
-  public Team getTeam() {
-    return team;
-  }
-
-  public void setTeam(Team team) {
-    this.team = team;
-  }
-
-  public String getPass() {
-    return pass;
-  }
-
-  public void setPass(String pass) {
-    this.pass = pass;
-  }
-
-  public boolean isAdmin() {
-    return isAdmin;
-  }
-
-  public void setAdmin(boolean admin) {
-    isAdmin = admin;
-  }
-
-  public boolean isManager() {
-    return isManager;
-  }
-
-  public void setManager(boolean manager) {
-    isManager = manager;
-  }
-
-  public Status getStatus() {
-    return status;
-  }
-
-  public void setStatus(Status status) {
-    this.status = status;
-  }
-
-  public Date getJoiningDate() {
-    return joiningDate;
-  }
-
-  public void setJoiningDate(Date joiningDate) {
-    this.joiningDate = joiningDate;
-  }
-
-  public Date getCreatedAt() {
-    return createdAt;
-  }
-
-  public void setCreatedAt(Date createdAt) {
-    this.createdAt = createdAt;
-  }
-
-  public Date getUpdatedAt() {
-    return updatedAt;
-  }
-
-  public void setUpdatedAt(Date updatedAt) {
-    this.updatedAt = updatedAt;
-  }
-
-  public UserProfile getUpdatedBy() {
-    return updatedBy;
-  }
-
-  public void setUpdatedBy(UserProfile updatedBy) {
-    this.updatedBy = updatedBy;
-  }
-
-
-  public UserProfile getCreatedBy() {
-    return createdBy;
-  }
-
-  public void setCreatedBy(UserProfile createdBy) {
-    this.createdBy = createdBy;
-  }
-
-
-  @Override
-  public String toString() {
-    return "User{" +
-        "id=" + id +
-        ", userName='" + userName + '\'' +
-        ", pin='" + pin + '\'' +
-        ", phone='" + phone + '\'' +
-        ", email='" + email + '\'' +
-        ", department='" + department + '\'' +
-        ", team=" + team +
-        ", pass='" + pass + '\'' +
-        ", isAdmin=" + isAdmin +
-        ", isManager=" + isManager +
-        ", status=" + status +
-        ", joiningDate=" + joiningDate +
-        ", createdAt=" + createdAt +
-        ", updatedAt=" + updatedAt +
-        ", createdBy=" + createdBy +
-        ", updatedBy=" + updatedBy +
-        '}';
-  }
 
 }
